34cbb2c8871f8e49500f4fafd2d2c1610ea4c957
hstevenscli <16stevehun98@gmail.com>
2025-02-26 10:10:44 -0700

    moving vanilla jscript to vue file

--- frontend/vueapp.js
+++ frontend/vueapp.js
@@ -55,9 +55,49 @@ Vue.createApp({
         },
         login: function () {
             console.log("Logging in");
+        },
+        getTextareaLines: function () {
+            let text = document.querySelector(".textarea").value;
+            let arr = text.split("\n");
+            return arr
+        },
+        linesToObject: function () {
+            let program = {user: user, instructions: []}
+            let lines = getTextareaLines();
+
+            for (let i = 0; i < lines.length; i++) {
+                let standbyobj = {instruction: "", args: []}
+                let line = lines[i].split(" ");
+                standbyobj.instruction = line[0];
+                for (let j = 1; j < line.length; j++) {
+                    let copy = line[j]
+                    if (!isNaN(Number(copy))) {
+                        standbyobj.args.push(Number(line[j]))
+                    } else {
+                        standbyobj.args.push(line[j])
+                    }
+                }
+                program.instructions.push(standbyobj);
+            }
+            return program;
+        },
+        submitProgram: async function () {
+            let program = this.linesToObject();
+            let jsonProgram = JSON.stringify(program);
+            let response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: jsonProgram
+            });
+            if (response.ok) {
+                let json = await response.json();
+                console.log("Response:", json)
+            } else {
+                alert("HTTP-Error: " + response.status);
+            }
         }
-
-
     },
     created: async function () {
         this.helloThere();
